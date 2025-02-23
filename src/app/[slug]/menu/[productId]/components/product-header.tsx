70072e466f6bca42846cc9c5c1dbaf26e36a486b
vettorellov <hugo.vettorello@acovisa.com.br>
2025-02-23 12:03:41 -0300

    feat: add product details page

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Product } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
@@ -7,34 +7,31 @@ import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
-
 interface ProductHeaderProps {
-    product: Pick<Product, 'name' | 'imageUrl'>
+  product: Pick<Product, "name" | "imageUrl">;
 }
 
-const ProductHeader = ({product} : ProductHeaderProps) => {
-
-    const router  = useRouter()
-
-    const handleBackClick = () => router.back()
-
-    return ( 
-
-        <div className="relative w-full h-[300px]">
-        <Button
-            variant="secondary"
-            size="icon"
-            className="absolute left-4 top-4 z-50 rounded-full"
-            onClick={handleBackClick}
+const ProductHeader = ({ product }: ProductHeaderProps) => {
+  const router = useRouter();
+  const handleBackClick = () => router.back();
+  return (
+    <div className="relative min-h-[300px] w-full">
+      <Button
+        variant="secondary"
+        size="icon"
+        className="absolute left-4 top-4 z-50 rounded-full"
+        onClick={handleBackClick}
       >
         <ChevronLeftIcon />
       </Button>
+
       <Image
-                src={product.imageUrl}
-                alt={product.name}
-                fill
-                className="object-contain"
-            />
+        src={product.imageUrl}
+        alt={product.name}
+        fill
+        className="object-contain"
+      />
+
       <Button
         variant="secondary"
         size="icon"
@@ -42,11 +39,8 @@ const ProductHeader = ({product} : ProductHeaderProps) => {
       >
         <ScrollTextIcon />
       </Button>
+    </div>
+  );
+};
 
-            
-           
-        </div>
-     );
-}
- 
 export default ProductHeader;
--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
+import { formatCurrency } from "@/helpers/format-currency";
+
 interface ProductsProps {
     products: Product[]
 }
@@ -24,10 +26,7 @@ const Products = ({products }: ProductsProps) => {
                         </h3>
                         <p className="line-clamp-2 text-sm text-muted-foreground">{product.description}</p>
                         <p className="pt-3 text-sm font-semibold">
-                            {new Intl.NumberFormat("pt-br",{
-                                style: 'currency',
-                                currency: 'BRL'
-                            }).format(product.price)}
+                           {formatCurrency(product.price)}
                         </p>
                     </div>
 
