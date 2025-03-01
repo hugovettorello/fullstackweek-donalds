83161ee518df3b964fe8b32c5eda4643c1c5794e
vettorellov <hugo.vettorello@acovisa.com.br>
2025-03-01 14:00:27 -0300

    feat: ensure order can be created

--- src/app/[slug]/menu/components/finish-order-dialog.tsx
+++ src/app/[slug]/menu/components/finish-order-dialog.tsx
@@ -9,6 +9,13 @@ import { useForm } from "react-hook-form";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {PatternFormat} from "react-number-format"
+import { createOrder } from "../actions/create-order";
+import { useParams, useSearchParams } from "next/navigation";
+import { ConsumptionMethod } from "@prisma/client";
+import { useContext, useTransition } from "react";
+import { CartContext } from "../context/cart";
+import { toast } from "sonner";
+import { Loader2Icon } from "lucide-react";
 
 const formSchema = z.object({
     name: z.string().trim().min(1,{
@@ -29,6 +36,12 @@ interface FinishOrderDialogProps {
 }
 
 const FinishOrderDialog = ({open, onOpenChange} : FinishOrderDialogProps) => {
+    const {slug} = useParams<{slug: string}>()
+    const {products} = useContext(CartContext)
+    const searchParams = useSearchParams()
+
+    const [isPending, startTransition] = useTransition()
+
     const form = useForm<FormSchema>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -38,8 +51,25 @@ const FinishOrderDialog = ({open, onOpenChange} : FinishOrderDialogProps) => {
         shouldUnregister: true
     })
 
-    const onSubmit = (data : FormSchema) => {
-        console.log({data})
+    const onSubmit = async (data : FormSchema) => {
+        try{
+            const consumptionMethod = searchParams.get('consumptionMethod') as ConsumptionMethod
+            startTransition(async ()=> {
+                await createOrder({
+                    consumptionMethod,
+                    customerCpf: data.cpf,
+                    customerName: data.name,
+                    products,
+                    slug
+                })
+                onOpenChange(false);
+                toast.success("Pedido finalizado com sucesso.")
+            })
+            
+           
+        }catch(e){
+            console.error(e)
+        }
     }
 
     return ( 
@@ -85,7 +115,16 @@ const FinishOrderDialog = ({open, onOpenChange} : FinishOrderDialogProps) => {
                             />
                             
                         <DrawerFooter>
-                        <Button type="submit" variant="destructive" className="rounded-full">Finalizar</Button>
+                        <Button 
+                            type="submit" 
+                            variant="destructive" 
+                            className="rounded-full"
+                            disabled={isPending}
+                        >
+                            
+                            {isPending && <Loader2Icon className="animate-spin"/>}
+                            
+                            Finalizar</Button>
                         <DrawerClose asChild>
                             <Button variant="outline" className="w-full rounded-full">Cancelar</Button>
                         </DrawerClose>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
 import { CartProvider } from "./[slug]/menu/context/cart";
+import { Toaster } from "@/components/ui/sonner";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -26,6 +27,7 @@ export default function RootLayout({
           <CartProvider>
             {children}
           </CartProvider>
+          <Toaster/>
         </body>
     </html>
   );
